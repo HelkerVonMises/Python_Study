6929dc694008801c2d1dcbe1b95613216392bc35
HelkerVonMises <94910396+HelkerVonMises@users.noreply.github.com>
2022-03-02 21:07:57 -0300

    Create Pr03.py

---
+++ Pr03.py
@@ -0,0 +1,11 @@
+def main():
+	a_str = input("Digite o primeiro número: ")
+	b_str = input("Digite o segundo número: ")
+
+	a_int = int(a_str)
+	b_int = int(b_str)
+
+	multi = a_int * b_int
+
+	print("A multiplicação de", a_int, "*", b_int, "é igual a", multi)
+main()
